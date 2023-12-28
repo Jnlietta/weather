f255a1c7c18958d8cc1578dd25223bb9a72086d2
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-28 16:11:26 +0100

    Extract weatherData from func handleCityChange to local state

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,23 +1,25 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+  const [weatherData, setWeatherData] = useState('');
 
   const handleCityChange = useCallback(city => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b90a673343f695c945721acd1ffcf17f&units=metric`) //dzieki units=metric dane beda w st celsjusza a nie kelvina
     .then(res => res.json())
     .then(data => {
-      const weatherData = {
+      setWeatherData({
         city: data.name,
         temp: data.main.temp,
         icon: data.weather[0].icon,
         description: data.weather[0].main
-      };
-      console.log(weatherData);
+      });
     });
   }, []);
+
+  console.log(weatherData);
 
   return (
     <section>
