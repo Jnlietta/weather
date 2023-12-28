3d3793f30a1cec36f93665ff2a7c3454fb6224cc
Julia Glinka <julia.w.glinka@gmail.com>
2023-12-28 15:51:13 +0100

    Download data about weater from API

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -6,7 +6,11 @@ import { useCallback } from 'react';
 const WeatherBox = props => {
 
   const handleCityChange = useCallback(city => {
-    console.log('city:', city);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b90a673343f695c945721acd1ffcf17f&units=metric`) //dzieki units=metric dane beda w st celsjusza a nie kelvina
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
   }, []);
 
   return (
